0166328c82c92b6cf9a5b61df113a244721ffc9d
nbyrd9 <nianibyrd@gmail.com>
2021-06-06 10:37:56 -0400

    add Homepage

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+      <div>
+        <h1>Welcome to DogDaze!</h1>
+        <p> ----INSERT ADDITIONAL INFO HERE ---- </p>
+        </div>
+    );
+  };
+
+  export default Home;
