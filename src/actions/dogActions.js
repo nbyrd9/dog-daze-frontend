eff2934d090c78a036d30f3a3c3f07e859c4f6aa
nbyrd9 <nianibyrd@gmail.com>
2021-06-06 15:31:26 -0400

    additional changes

--- src/actions/dogActions.js
+++ src/actions/dogActions.js
@@ -3,26 +3,26 @@ export const getDogActions = (dogId) => {
        dispatch({type: "LOADING_ACTIVITIES"}) 
        return fetch(`http://localhost:3000/dogs/${dogId}`)
        .then(res => res.json())
-       .then(dog => dispatch({type: "ACTIVITIES_LOADED", payload: dog}))
+       .then(dog => dispatch({type: "ACTIONS_LOADED", payload: dog}))
        .catch(error => {
         throw(error);
         }) 
     }
 }
 
-export const addActivity = (activity, dogId) => {
+export const addAction = (action, dogId) => {
     return(dispatch) => {
-        dispatch({type: "ADD_ACTIVITY"}, activity)
+        dispatch({type: "ADD_action"}, action)
         return fetch(`http://localhost:3000/dogs/${dogId}/dog_actions`, {
             method: "POST",
-            body: JSON.stringify(activity),
+            body: JSON.stringify(action),
             headers: { 
                 'Content-Type':  'application/json'
             }
         })
         .then(res => res.json())
-        .then(activity => {
-            return dispatch({type: "ACTIVITY_ADDED", payload: activity})
+        .then(action => {
+            return dispatch({type: "ACTION_ADDED", payload: action})
         })
     }
 }
