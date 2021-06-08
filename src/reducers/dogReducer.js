d332af20055a51e8efbbd3c4ce44a66d8f3918f7
nbyrd9 <nianibyrd@gmail.com>
2021-06-08 13:05:28 -0400

    remove unused code

--- src/reducers/dogReducer.js
+++ src/reducers/dogReducer.js
@@ -3,30 +3,30 @@ export default function dogReducer(state = {dogs: [], value: ''}, action) {
         case "LOADING_DOGS":
             return {
                 ...state,
-                // loading: true
+               
             }
         case "DOGS_LOADED":
             return {
                 dogs: action.payload,
-                // loading: false
+             
             }
         case "ADD_DOG":
                 return {
                     ...state,
-                    // loading: true
+             
                 }
 
         case "DOG_ADDED":
             return {
                 ...state,
                 dogs: [...state.dogs, action.payload],
-                // loading: false
+      
             }  
             
         case "ADD_ACTION":
             return{
                 ...state,
-                // loading: true
+
             }
         case "ACTION_ADDED":
             
@@ -43,7 +43,7 @@ export default function dogReducer(state = {dogs: [], value: ''}, action) {
             return{
                 ...state,
                 dogs: [...dogs, newAction]
-                // loading: false
+              
             }
 
         default:
